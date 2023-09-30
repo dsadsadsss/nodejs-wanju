3d9d04a1077035f3409fcd8dc8ec62399b5f1bff
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-09-30 12:39:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
+## ===========================================设置各参数（不需要的可以删掉或者前面加# ）=============================================
+# export FLIE_PATH="${FLIE_PATH:-/tmp/}"
 
-## ==========设置各参数（不需要的可以删掉或者前面加# ）==================
-export FLIE_PATH='/tmp/'
-
-# 设置固定隧道参数 (如果设置，注意把前面的# 去掉，如果不设置，默认启用临时隧道)
-# export TOK='xxxxx'
-# export ARGO_DOMAIN='xxxxx'
+# 设置ARGO参数 (不设置默认使用临时隧道，如果设置把前面的#去掉)
+# export TOK='xxxx'
+# export ARGO_DOMAIN='xxxx'
 
 #设置哪吒参数(NEZHA_TLS='1'开启tls,设置其他关闭tls)
-export NEZHA_SERVER='xxxxx'
-export NEZHA_KEY='xxxxx'
+export NEZHA_SERVER='xxx'
+export NEZHA_KEY='xxx'
 export NEZHA_PORT='443'
 export NEZHA_TLS='1'
 
-# 下面设置UUID和路径，CF_IP是优选IP，SUB_NAME为节点名称
-export UUID='fd80f56e-93f3-4c85-b2a8-c77216c509a7'
-export VPATH='vls'
-export CF_IP='cdn.xn--b6gac.eu.org'
-export SUB_NAME='bbn'
+#设置app参数（默认x-ra-y参数，如果你更该了下载地址，需要修改UUID和VPATH）
+export UUID='xxx'
+export VPATH='xxx'
+export CF_IP='xxx'
+export SUB_NAME='deepnote'
 
 #设置订阅上传地址
-export SUB_URL='xxxxxx'
+export SUB_URL='xxxxx'
 
-# 设置启动玩具原程序，senver.jar 原启动文件改名
-# export JAR_SH='java -Xms128M -XX:MaxRAMPercentage=95.0 -jar senver.jar'
-# 默认启动原游戏后，不显示日志
+#设置端口（玩具平台不需要）
+# export SERVER_PORT='8080'
 
-## ===========================================参数设置完毕，启动程序===============================
+# 设置启动玩具平台原程序，senver.jar 原启动文件改名
+# export JAR_SH='java -Xms128M -XX:MaxRAMPercentage=95.0 -jar senver.jar --port=46522'
+
 if command -v curl &>/dev/null; then
         DOWNLOAD_CMD="curl -sL"
     # Check if wget is available
