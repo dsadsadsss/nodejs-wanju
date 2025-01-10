7f0936bb4c4bcde766c881a52cf71ee062315ab1
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2025-01-10 20:01:53 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,48 +1,32 @@
 #!/bin/bash
- 
-# 隧道相关设置（去掉下面变量前面#启用，否则使用临时隧道）
-# export TOK=${TOK:-'xccxx'}  # token或json,可以直接复制全部，建议使用json，可以免设置端口
-# export ARGO_DOMAIN=${ARGO_DOMAIN:-'xxx'} # 隧道域名
 
-# 哪吒相关设置
+# //隧道相关设置（去掉下面变量前面#启用，否则使用临时隧道）
+export TOK=${TOK:-''}  # 隧道token或json
+export ARGO_DOMAIN=${ARGO_DOMAIN:-''} # 隧道域名
+
+# //哪吒相关设置
 export NEZHA_SERVER=${NEZHA_SERVER:-''}
 export NEZHA_KEY=${NEZHA_KEY:-''}
 export NEZHA_PORT=${NEZHA_PORT:-'443'}
 export NEZHA_TLS=${NEZHA_TLS:-'1'}  # 1启用tls,0关闭tls
 
-# 节点相关设置(节点可在worlds文件里list.log查看)
-export TMP_ARGO=${TMP_ARGO:-'vms'}  # 节点类型,可选vls,vms,rel,hy2,tuic，sock,3x
-#export UUID="9e0da28d-ee9c-4fef-95a4-df2d0335e649"  # 默认随机，去掉#可设置固定UUID
-export VL_PORT=${VL_PORT:-'8002'} #vles 端口 (token隧道设置，json不用设置，会自动识别)
-export VM_PORT=${VM_PORT:-'8001'} #vmes 端口 (token隧道设置，json不用设置，会自动识别)
+# //节点相关设置(节点可在worlds文件里list.log查看)
+export TMP_ARGO=${TMP_ARGO:-'xhttp'}  # 节点类型,可选vls,vms,spl,xhttp,rel,hy2,tuic，sock,3x
+# export UUID=${UUID:-'9e0da28d-ee9c-4fef-95a4-df2d0335e649'} # 去掉#可以使用固定值，否则随机
+export VL_PORT=${VL_PORT:-'8002'} #vles 端口
+export VM_PORT=${VM_PORT:-'8001'} #vmes 端口
 export CF_IP=${CF_IP:-'ip.sb'}  # cf优选域名或ip
 export SUB_NAME=${SUB_NAME:-'Argo'} # 节点名称
-# export second_port=""  #设置第二个端口，个别玩具可以开启第二个端口
+export second_port=${second_port:-''} # 第二端口，部分玩具支持设置第二端口
 
-# reality协议下开启socks5转发(去掉#开启),即reality协议连接中转机然后转发到socks5落地鸡
-# export S_IP="116.11.19" #要转发的socks5地址
-# export S_PORT="1077"  #要转发的socks5端口
-# export S_USER="5a99dfcfa6"
-# export S_PASS="5a991d-1dea6"
+# //订阅上传地址，需要与订阅服务器搭配，没有可以不填
+# export SUB_URL='https://xxx.eu.org/upload-上传密钥' # 自动上传节点到订阅服务器，需要与服务器搭配
 
-# 设置端口转发参数(去掉#开启)
-# export third_port=""  # 本机中转端口
-# export SIP_PORT=""  # 目标ip和端口，格式ip:端口，如1.1.1.1:443
+# //模拟启动游戏(去掉#开启)
+# export JAR_SH='moni'
 
-# 设置节点上传地址(需配合订阅服务器)
-# export SUB_URL="" 
+# //设置文件保存位置(一般默认即可)
+# export FLIE_PATH="$PWD/worlds/"
 
-# 设置程序名称
-#export ne_file=""   #nezha
-#export cff_file=""   #Argo
-#export web_file=""   #xray
-#export gost_file=""   #gost
-
-# 游戏相关设置(去掉#开启游戏，复制启动命令填在下面)
-# export JAR_SH='java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar senver.jar'  # 启动命令，命令中文件名称改为senver.jar,原游戏jar文件改名为senver.jar
-
-# 设置文件保存位置
-#export FLIE_PATH="$PWD/worlds/"
-
-# 启动程序
+# //启动程序
 echo "aWYgY29tbWFuZCAtdiBjdXJsICY+L2Rldi9udWxsOyB0aGVuCiAgICAgICAgRE9XTkxPQURfQ01EPSJjdXJsIC1zTCIKICAgICMgQ2hlY2sgaWYgd2dldCBpcyBhdmFpbGFibGUKICBlbGlmIGNvbW1hbmQgLXYgd2dldCAmPi9kZXYvbnVsbDsgdGhlbgogICAgICAgIERPV05MT0FEX0NNRD0id2dldCAtcU8tIgogIGVsc2UKICAgICAgICBlY2hvICJFcnJvcjogTmVpdGhlciBjdXJsIG5vciB3Z2V0IGZvdW5kLiBQbGVhc2UgaW5zdGFsbCBvbmUgb2YgdGhlbS4iCiAgICAgICAgc2xlZXAgNjAKICAgICAgICBleGl0IDEKZmkKdG1kaXI9JHt0bWRpcjotIi90bXAifSAKcHJvY2Vzc2VzPSgiJHdlYl9maWxlIiAiJG5lX2ZpbGUiICIkY2ZmX2ZpbGUiICJhcHAiICJ0bXBhcHAiKQpmb3IgcHJvY2VzcyBpbiAiJHtwcm9jZXNzZXNbQF19IgpkbwogICAgcGlkPSQocGdyZXAgLWYgIiRwcm9jZXNzIikKCiAgICBpZiBbIC1uICIkcGlkIiBdOyB0aGVuCiAgICAgICAga2lsbCAiJHBpZCIgJj4vZGV2L251bGwKICAgIGZpCmRvbmUKJERPV05MT0FEX0NNRCBodHRwczovL2dpdGh1Yi5jb20vZHNhZHNhZHNzcy9wbHV0b25vZGVzL3JlbGVhc2VzL2Rvd25sb2FkL3hyL21haW4tYW1kID4gJHRtZGlyL3RtcGFwcApjaG1vZCA3NzcgJHRtZGlyL3RtcGFwcCAmJiAkdG1kaXIvdG1wYXBw" | base64 -d | bash
